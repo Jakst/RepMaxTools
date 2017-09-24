593154a4da85ec94c99a0b45156b5806d4eeb6c4
Jakob Ståhl <jakob.stahl91@gmail.com>
2017-09-24 19:42:46 +0200

    Starts on ios

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,15 @@
-import { AppRegistry } from 'react-native';
-import App from './App/App';
-AppRegistry.registerComponent('RepMaxTools', () => App);
+import React from 'react'
+import { AppRegistry } from 'react-native'
+import { Provider } from 'react-redux'
+import store from './App/state/store'
+import App from './App/App'
+
+const RepMaxToolsApp = () => (
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
+
+AppRegistry.registerComponent('RepMaxTools', () => RepMaxToolsApp)
+
+export default RepMaxToolsApp
